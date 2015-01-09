4fdb346fc2910471ce68ad6bf6e269b67def45af
Szymon <s.jarosz@appswith.us>
2015-01-09 16:52:20 +0100

    Revert "OkHttp version 2"
    This reverts commit 9cff775cbf9934d741e971e89f5c070c088ee252.

--- src/com/android/volley/toolbox/OkHttpStack.java
+++ src/com/android/volley/toolbox/OkHttpStack.java
@@ -17,33 +17,31 @@
  */
 package com.android.volley.toolbox;
 
+import com.android.volley.toolbox.HurlStack;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+ 
 /**
  * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which
  * uses OkHttp as its transport.
  */
 public class OkHttpStack extends HurlStack {
-    private final OkUrlFactory okUrlFactory;
-
-    public OkHttpStack() {
-        this(new OkUrlFactory(new OkHttpClient()));
+  private final OkHttpClient client;
+ 
+  public OkHttpStack() {
+    this(new OkHttpClient());
+  }
+ 
+  public OkHttpStack(OkHttpClient client) {
+    if (client == null) {
+      throw new NullPointerException("Client must not be null.");
     }
-
-    public OkHttpStack(OkUrlFactory okUrlFactory) {
-        if (okUrlFactory == null) {
-            throw new NullPointerException("Client must not be null.");
-        }
-        this.okUrlFactory = okUrlFactory;
-    }
-
-    @Override
-    protected HttpURLConnection createConnection(URL url) throws IOException {
-        return okUrlFactory.open(url);
-    }
+    this.client = client;
+  }
+ 
+  @Override protected HttpURLConnection createConnection(URL url) throws IOException {
+    return client.open(url);
+  }   
 }
